3c5049c19830b43a69f7e8e1710258a8843a2a8c
Luis Ruiz Pavón <lruiz@plainconcepts.com>
2017-09-22 09:00:38 +0200

    Initial commit

---
+++ EFCore2Playgrounds/Migrations/20170922065639_Initial.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EFCore2Playgrounds.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_States_StateId",
+                        column: x => x.StateId,
+                        principalTable: "States",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderCategories",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderCategories", x => new { x.OrderId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_OrderCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderCategories_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderCategories_CategoryId",
+                table: "OrderCategories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StateId",
+                table: "Orders",
+                column: "StateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderCategories");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "States");
+        }
+    }
+}
---
+++ EFCore2Playgrounds/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore2Playgrounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new MyDbContext())
+            {
+                dbContext.Database.Migrate();
+                var draftState = new State { Id = 1, Name = "Draft" };
+                var cancelState = new State { Id = 2, Name = "Cancel" };
+                dbContext.States.Add(draftState);
+                dbContext.States.Add(cancelState);
+                dbContext.SaveChanges();
+                var category = new Category { Name = "Vip" };
+                dbContext.Categories.Add(category);
+                dbContext.SaveChanges();
+                var order = new Order();
+                order.AddCategory(category);
+                dbContext.Orders.Add(order);
+                dbContext.SaveChanges();
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        private int StateId;
+        private State _state;
+        private State State => _state;
+        private List<OrderCategory> _orderCategories = new List<OrderCategory>();
+        private IReadOnlyCollection<OrderCategory> OrderCategories => _orderCategories.ToList();
+
+        public Order()
+        {
+            StateId = 1;
+        }
+
+        public void Cancel()
+        {
+            StateId = 2;
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _orderCategories.Select(x => x.Category);
+        }
+
+        public void AddCategory(Category category)
+        {
+            _orderCategories.Add(new OrderCategory {Category = category, Order = this});
+        }
+    }
+
+    public class State
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class OrderCategory
+    {
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<OrderCategory> OrderCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=efcoreplaygrounds;Trusted_Connection=yes",
+                builder => builder.MigrationsAssembly("EFCore2Playgrounds"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<State>()
+                .Property(x => x.Id).ValueGeneratedNever();
+            modelBuilder.Entity<Order>()
+                .HasOne(typeof(State), nameof(State))
+                .WithMany()
+                .HasForeignKey("StateId");
+            modelBuilder.Entity<OrderCategory>()
+                .HasKey(x => new {x.OrderId, x.CategoryId});
+            modelBuilder.Entity<OrderCategory>()
+                .HasOne(typeof(Order), nameof(Order))
+                .WithMany()
+                .HasForeignKey("OrderId");
+            modelBuilder.Entity<OrderCategory>()
+                .HasOne(typeof(Category), nameof(Category))
+                .WithMany()
+                .HasForeignKey("CategoryId");
+        }
+    }
+}
